351e57a4cea58b947fbb75513c94f153b19ff20c
iiiiii1wepfj <itaykotik@gmail.com>
2020-11-29 09:44:54 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,24 @@
+from pyrogram import Client, filters
+import asyncio
+
+
+api_id = your api id
+api_hash = 'your api hash'
+token = "bot token"
+
+
+
+Client = Client("unpinbot", api_id, api_hash, bot_token=token)
+
+
+@Client.on_message(filters.linked_channel)
+async def autopin(Client, Message):
+    await Client.unpin_chat_message(
+    Message.chat.id
+    )
+    
+
+
+
+
+Client.run()
