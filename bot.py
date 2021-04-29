e2b70828bf3762c36fd8a79764921743017cf046
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-04-29 15:18:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,16 @@
 from pyrogram import Client, filters
 
-
 api_id = your api id
 api_hash = 'your api hash'
 token = "bot token"
-
 Client = Client("unpinbot", api_id, api_hash, bot_token=token)
-
 
 @Client.on_message(filters.linked_channel)
 async def autopin(Client, Message):
     await Message.unpin()
-    
-    
+
 @Client.on_message(filters.private)
 async def privatemsg(client, Message):
-  await  Message.reply("""text""")
+  await Message.reply("""text""")
 
-    
 Client.run()
