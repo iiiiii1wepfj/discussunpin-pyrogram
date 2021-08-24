13e2620756b83f22d822e7499217f97f17d0a270
iiiiii1wepfj <18629298+iiiiii1wepfj@users.noreply.github.com>
2021-08-24 10:09:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from pyrogram import Client, filters
 
-api_id = your api id
-api_hash = "your api hash"
-token = "bot token"
+api_id: int = your api id
+api_hash: str = "your api hash"
+token: str = "bot token"
 Client = Client("unpinbot", api_id, api_hash, bot_token=token)
 
 
